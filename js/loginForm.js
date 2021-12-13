5e6c0c7cb2e846ce723e2cb192d50b62262a14b0
lavydeveloper <lavy.prof@gmail.com>
2021-12-13 13:01:24 +0100

    loginForm

--- js/loginForm.js
+++ js/loginForm.js
@@ -11,7 +11,7 @@ export let renderLoginForm = () => {
 
             event.preventDefault();
     
-            let url = form.action;S
+            let url = loginForm.action;
             let data = new FormData(loginForm);
             data.append("fingerprint", getID());
     
