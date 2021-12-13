2234232b474ab05219e993f87828a9b7d2938119
lavydeveloper <lavy.prof@gmail.com>
2021-12-13 12:55:53 +0100

    burgerMenu

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,16 +1,19 @@
 export const burgerMenu = () => {
     
     const menuBtn = document.querySelector('.menu-btn');
+    let menuOpen = false;
 
-let menuOpen = false;
-menuBtn.addEventListener('click', ()=> {
-    if(!menuOpen) {
-        menuBtn.classList.add('open');
-        menuOpen=true;
+    if(menuBtn){
+
+        menuBtn.addEventListener('click', ()=> {
+            if(!menuOpen) {
+                menuBtn.classList.add('open');
+                menuOpen=true;
+            }
+            else {
+                menuBtn.classList.remove('open');
+                menuOpen=false;
+            }
+        });
     }
-    else {
-        menuBtn.classList.remove('open');
-        menuOpen=false;
-    }
-});
 }
