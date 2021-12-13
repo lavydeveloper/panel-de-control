88a1658531bcd831379803d008d4acc9061cb3f1
lavydeveloper <lavy.prof@gmail.com>
2021-12-13 12:59:00 +0100

    tabs

--- js/tabs.js
+++ js/tabs.js
@@ -4,31 +4,33 @@ export const tabs = () =>
     const tabContents = document.querySelectorAll(".tab-content");
 
     /*Seleccionar cada elemento del array*/
-    tabButtons.forEach(tabButton => {
+    if(tabButtons){
+        tabButtons.forEach(tabButton => {
 
-        /*Evento click a cada uno de los elementos*/
-        tabButton.addEventListener("click", () =>{
-
-            /*Seleccionar todos los elementos con la clase .tab-active*/
-            let activeElements = document.querySelectorAll(".tab-active");
-            /*Seleccionar cada elemento del array*/
-            activeElements.forEach(activeElement =>{
-                /*A cada uno de los elementos con la clase .tab-active le quitamos la clase .tab-active*/
-                activeElement.classList.remove("tab-active");
-            });
-            /*A cada uno de los elementos del array le añadimos la clase .tab-active*/
-            tabButton.classList.add("tab-active");
-        
-            /*Seleccionar cada elemento del array*/
-            tabContents.forEach(tabContent => {
-                /*Si el dataset de tabContent es igual al dataset del tabButton...*/
-                if(tabContent.dataset.tab == tabButton.dataset.tab)
-                {
-                    /*Le añadimos a cada elemento del array la clase .tab-active*/
-                    tabContent.classList.add("tab-active");
-                }
+            /*Evento click a cada uno de los elementos*/
+            tabButton.addEventListener("click", () =>{
+    
+                /*Seleccionar todos los elementos con la clase .tab-active*/
+                let activeElements = document.querySelectorAll(".tab-active");
+                /*Seleccionar cada elemento del array*/
+                activeElements.forEach(activeElement =>{
+                    /*A cada uno de los elementos con la clase .tab-active le quitamos la clase .tab-active*/
+                    activeElement.classList.remove("tab-active");
+                });
+                /*A cada uno de los elementos del array le añadimos la clase .tab-active*/
+                tabButton.classList.add("tab-active");
+            
+                /*Seleccionar cada elemento del array*/
+                tabContents.forEach(tabContent => {
+                    /*Si el dataset de tabContent es igual al dataset del tabButton...*/
+                    if(tabContent.dataset.tab == tabButton.dataset.tab)
+                    {
+                        /*Le añadimos a cada elemento del array la clase .tab-active*/
+                        tabContent.classList.add("tab-active");
+                    }
+                });
             });
         });
-    });
+    }
 }
 
